c574a4ff5be0a78c020b07c8f7085a39d597d85d
Shawn Thompson <plansmash@me.com>
2024-06-26 20:10:54 -0400

    Update home page

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Lifewithshortarms",
+	url: "https://lifewithshortarms.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Christen's mission is clear: to inspire, educate, and advocate for accessibility, diversity, and inclusion on global platforms.  With a website as the landing page for all information, this will be the hub of all outreach.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Christen Roos",
+		email: "lifewithshortarms@gmail.com",
+		url: "https://lifewithshortarms.com#about"
 	}
 }
