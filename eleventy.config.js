5736dd0166ffa2a7fd7465caa55313aa59ca74e8
Shawn Thompson <plansmash@me.com>
2024-06-27 19:30:22 -0400

    add CMS

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,6 +15,7 @@ module.exports = function(eleventyConfig) {
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
 		"./public/": "/",
+		"./admin": "/admin",
 	});
 
 	// Run Eleventy when these files change:
